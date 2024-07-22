da7920bcf84f740cbad0071098b54be6f9fe4c52
Biblioklept <61851290+Biblioklept@users.noreply.github.com>
2024-07-21 21:19:14 -0400

    Update 2.2.3

--- Sniff's Utils 2/lua/disablecloakercamera.lua
+++ Sniff's Utils 2/lua/disablecloakercamera.lua
@@ -1,7 +1,7 @@
 -- ORIGINAL MOD: https://modworkshop.net/mod/16487
 
 dofile(ModPath .. "core.lua")
-if SniffUtils:GetOption("no_cloaker_camera") then
+if SniffUtils:GetOption("disable_cloaker_camera") then
 function FPCameraPlayerBase:clbk_aim_assist(col_ray)
 	if managers.controller:get_default_wrapper_type() ~= "pc" and managers.user:get_setting("aim_assist") then
 		self:_start_aim_assist(col_ray, self._aim_assist)
