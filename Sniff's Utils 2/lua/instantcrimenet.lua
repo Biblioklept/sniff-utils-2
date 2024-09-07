24aa9d63130941041ea5865c867988a279c32735
Biblioklept <61851290+Biblioklept@users.noreply.github.com>
2024-09-07 16:13:24 -0400

    Tweaked active_job_time value

--- Sniff's Utils 2/lua/instantcrimenet.lua
+++ Sniff's Utils 2/lua/instantcrimenet.lua
@@ -1,7 +1,7 @@
 Hooks:PostHook( GuiTweakData , "init" , "cheesewallrandomname" , function( self , params )
 	self.crime_net.job_vars = {
 		max_active_jobs = 30, --was 10
-		active_job_time = 25,
+		active_job_time = 10, -- was 25
 		new_job_min_time = 0.01, --was 1,5
 		new_job_max_time = 0.02,
 		refresh_servers_time = SystemInfo:platform() == Idstring("PS4") and 10 or 5,
